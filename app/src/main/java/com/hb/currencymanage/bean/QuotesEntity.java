1546167d3e8a10d933837a1f9312898be0752a94
gds <995804286@qq.com>
2018-04-17 21:14:01 +0800

    gds

---
+++ app/src/main/java/com/hb/currencymanage/bean/QuotesEntity.java
@@ -0,0 +1,12 @@
+package com.hb.currencymanage.bean;
+
+/**
+ * Created by Administrator on 2018/4/17.
+ */
+
+public class QuotesEntity {
+
+    public float buy;
+
+
+}
--- app/src/main/java/com/hb/currencymanage/ui/fragment/QuotesFragment.java
+++ app/src/main/java/com/hb/currencymanage/ui/fragment/QuotesFragment.java
@@ -1,13 +1,22 @@
 package com.hb.currencymanage.ui.fragment;
 
 import com.hb.currencymanage.R;
+import com.hb.currencymanage.bean.QuotesEntity;
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,6 +25,13 @@ import butterknife.ButterKnife;
 
 public class QuotesFragment extends BaseFragment
 {
+
+
+    @BindView(R.id.recycleView)
+    RecyclerView recyclerView;
+
+    private List<QuotesEntity> quotesEntityList;
+
     public static QuotesFragment getInstance()
     {
         QuotesFragment sf = new QuotesFragment();
@@ -33,8 +49,37 @@ public class QuotesFragment extends BaseFragment
     public View onCreateView(LayoutInflater inflater,
             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
+
+
         View v = inflater.inflate(R.layout.fragment_quotes, null);
         ButterKnife.bind(this, v);
+        initData();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new CommonAdapter(getActivity(),R.layout.quotes_item,quotesEntityList) {
+            @Override
+            protected void convert(ViewHolder holder, Object o, int position) {
+
+               holder.setText(R.id.sale_num, "12");
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+        });
+
         return v;
+    }
+
+    private void initData() {
+        if(quotesEntityList == null){
+            quotesEntityList=new ArrayList<>();
+        }
+
+
+        for(int i=0;i<10;i++){
+
+            quotesEntityList.add(new QuotesEntity());
+        }
     }
 }
