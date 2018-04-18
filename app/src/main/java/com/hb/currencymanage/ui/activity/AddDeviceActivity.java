da2607c03a143b09366deeafa70dafedc18182e6
gds <995804286@qq.com>
2018-04-18 16:30:01 +0800

    gds

---
+++ app/src/main/java/com/hb/currencymanage/ui/activity/AddDeviceActivity.java
@@ -0,0 +1,15 @@
+package com.hb.currencymanage.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hb.currencymanage.R;
+
+public class AddDeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_device);
+    }
+}
---
+++ app/src/main/java/com/hb/currencymanage/ui/activity/DeviceDetailActivity.java
@@ -0,0 +1,29 @@
+package com.hb.currencymanage.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hb.currencymanage.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DeviceDetailActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_detail);
+        
+        ButterKnife.bind(this);
+    }
+    
+    @OnClick(R.id.back)
+    void back()
+    {
+        finish();
+    }
+    
+}
