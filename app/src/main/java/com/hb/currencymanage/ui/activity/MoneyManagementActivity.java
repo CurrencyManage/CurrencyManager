0bdf2c4c514f929f1d9e40350c003cb799ed67e6
gds <995804286@qq.com>
2018-04-18 22:13:23 +0800

    gds

---
+++ app/src/main/java/com/hb/currencymanage/ui/activity/MoneyManagementActivity.java
@@ -0,0 +1,64 @@
+package com.hb.currencymanage.ui.activity;
+
+import android.os.Bundle;
+
+import com.hb.currencymanage.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MoneyManagementActivity extends BaseActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_money_management);
+        ButterKnife.bind(this);
+
+
+
+    }
+
+
+    @OnClick(R.id.moneyrechargelayout)
+    void moneyrechargelayout()
+    {
+
+        changeActivity(MoneyRechargeActivity.class);
+
+    }
+
+    @OnClick(R.id.widthdrawlayout)
+    void widthdrawlayout()
+    {
+
+        changeActivity(WithDrawActivity.class);
+
+    }
+
+    @OnClick(R.id.emerygencylayout)
+    void emerygencylayout()
+    {
+
+        changeActivity(EmergencyActivity.class);
+
+    }
+
+
+    @OnClick(R.id.back)
+    void back()
+    {
+
+       finish();
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/hb/currencymanage/ui/activity/MoneyRechargeActivity.java
@@ -0,0 +1,34 @@
+package com.hb.currencymanage.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hb.currencymanage.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MoneyRechargeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_recharge);
+
+        ButterKnife.bind(this);
+
+    }
+
+
+
+    @OnClick(R.id.back)
+    void back()
+    {
+        finish();
+    }
+
+
+
+
+
+}
