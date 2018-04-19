54f321df717b69bef33903f03815845d065c2319
gds <995804286@qq.com>
2018-04-19 20:15:40 +0800

    gds

--- app/src/main/java/com/hb/currencymanage/ui/activity/TellUsActivity.java
+++ app/src/main/java/com/hb/currencymanage/ui/activity/TellUsActivity.java
@@ -1,12 +1,30 @@
 package com.hb.currencymanage.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.hb.currencymanage.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class TellUsActivity extends BaseActivity {
+
+
+    private List<String> list;
+
+    private ArrayAdapter<String> adapter;
+
+    @BindView(R.id.spDwon)
+    Spinner spDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +32,36 @@ public class TellUsActivity extends BaseActivity {
         setContentView(R.layout.activity_tell_us);
 
         ButterKnife.bind(this);
+
+        /*设置数据源*/
+        list=new ArrayList<String>();
+        list.add("北京");
+        list.add("上海");
+        list.add("广州");
+        list.add("深圳");
+        /*新建适配器*/
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+
+        /*adapter设置一个下拉列表样式，参数为系统子布局*/
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        /*spDown加载适配器*/
+        spDown.setAdapter(adapter);
+
+        /*soDown的监听器*/
+        spDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+               // String cityName=adapter.getItem(position);   //获取选中的那一项
+                //tvShow.setText("您选择的城市是"+cityName);
+                Log.e("tag",spDown.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
