08a687dfae45cff7207646b9e16f9435e0c50a84
gds <995804286@qq.com>
2018-04-21 17:23:52 +0800

    gds

--- app/src/main/java/com/hb/currencymanage/net/BaseObserver.java
+++ app/src/main/java/com/hb/currencymanage/net/BaseObserver.java
@@ -3,7 +3,7 @@ package com.hb.currencymanage.net;
 /**
  * Created by Administrator on 2018/4/21.
  */
-
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.widget.Toast;
 import com.hb.currencymanage.bean.ResultData;
@@ -12,16 +12,21 @@ import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+
+
+
 public abstract class BaseObserver<T> implements Observer<ResultData<T>> {
 
 
     private Context mContext;
 
+    private static ProgressDialog pd;
 
     public BaseObserver(Context context, boolean flag){
         this.mContext=context.getApplicationContext();
         if(flag){
             //进度框
+           // pd=ProgressDialog.show(context,"","正在加载");
 
         }
 
